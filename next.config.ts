fe799146a5b5253451918eafaca890106fb1d04a
Eduardo Santos <eduardosantos_777@icloud.com>
2025-08-13 03:01:35 -0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // Add your Next.js configuration options here
 };
 
 export default nextConfig;
